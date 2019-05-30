56fe057a1085553c74cc06d0bb85e6bc7977d7f4
peckw1 <peckw1@gmail.com>
2019-05-30 15:10:50 -0700

    new branch

---
+++ src/main/java/com/ackee/controller/ackee/AckeeApplication.java
@@ -0,0 +1,13 @@
+package com.ackee.controller.ackee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AckeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AckeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ackee/controller/ackee/AckeeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ackee.controller.ackee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AckeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
